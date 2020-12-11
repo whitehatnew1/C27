890b53169f251dbfb34c38e4981be1d0349a198e
whitehatnew1 <whitehatnewstudent1@gmail.comgit config -- global user.name whitehatnew1>
2020-12-11 07:39:42 +0530

    created constraint bodies

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +17,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    constrainedLog = new Log( 230, 180,80, PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -35,6 +37,15 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    var options={
+        bodyA:bird.body,
+        bodyB:constrainedLog.body,
+        stiffness:0.04,
+        length:10
+    }
+    var chain =Constraint.create(options);
+    World.add(world,chain);
+    
 
 }
 
@@ -61,4 +72,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+    strokeWeight(4);
+    line (bird.body.position.x,bird.body.position.y, constrainedLog.body.position.x, constrainedLog.body.position.y)
 }
